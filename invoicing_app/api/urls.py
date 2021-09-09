4723e1ee852fedfe19f161fb4132659b4007c651
ismailfarisi <ismailfarisi@gmail.com>
2021-09-09 15:42:16 +0400

    view

--- invoicing_app/api/urls.py
+++ invoicing_app/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns =[
     path('',views.getRoutes),
-    path('invoice/',views.InvoiceList.as_view()),
+    path('invoices/',views.InvoiceListView.as_view()),
+    path('invoices/<int:pk>/',views.InvoiceView.as_view()),
+    path('companies/',views.CompanyListView.as_view()),
+    path('companies/<int:pk>/',views.CompanyView.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- invoicing_app/api/views.py
+++ invoicing_app/api/views.py
@@ -3,8 +3,9 @@ from django.http.response import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import InvoiceProduct,Invoice
-from .serialzers import InvoiceSerializer , InvoiceProductSerializer
+from .models import Company, InvoiceProduct,Invoice
+from .serialzers import CompanySerializer, InvoiceSerializer , InvoiceProductSerializer
+from rest_framework import serializers, status
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -17,7 +18,7 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-class InvoiceList(APIView):
+class InvoiceListView(APIView):
     def get(self,request):
         invoice = Invoice.objects.all()
         serializer = InvoiceSerializer(invoice, many=True)
@@ -28,7 +29,72 @@ class InvoiceList(APIView):
         serializer= InvoiceSerializer(data=invoice)
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer._errors(Http404))
+
+class InvoiceView(APIView):
+
+    def get_object(self,pk):
+        try:
+            return Invoice.objects.get(pk=pk)
+        except Invoice.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,pk, format=None):
+        invoice = self.get_object(pk)
+        serializer = InvoiceSerializer(invoice)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        invoice = self.get_object(pk)
+        serializer= InvoiceSerializer(invoice,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer._errors(Http404))
+    
+    def delete(self, request, pk, format=None):
+        invoice = self.get_object(pk)
+        invoice.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
+class CompanyListView(APIView):      
+
+    def get(self,request):
+        invoice = Company.objects.all()
+        serializer = CompanySerializer(invoice, many=True)
+        return Response(serializer.data)
+
+    def post(self,request,response):
+        invoice = Company.objects.create(request.data)
+        serializer= CompanySerializer(data=invoice)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer._errors(Http404))
+
+class CompanyView(APIView):
+    def get_object(self,pk):
+        try:
+            return Company.objects.get(pk=pk)
+        except Company.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,pk):
+        company = self.get_object(pk)
+        serializer = CompanySerializer(company,many=False)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        company = self.get_object(pk)
+        serializer= CompanySerializer(company,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer._errors(Http404))
+    
+    def delete(self, request, pk, format=None):
+        company = self.get_object(pk)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
